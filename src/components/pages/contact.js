bd6fdf70d2804614a3a6b15344706f97eb00891e
Lillian Dixon <dixon.lillian92@gmail.com>
2019-02-25 04:58:32 -0700

    pages

---
+++ src/components/pages/contact.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default function(){
+    return(
+        
+    )
+}import React, {Component} from 'react';
+
+export default function(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
